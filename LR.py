5b8d5f9a3b4ea94fa0733c8af96315abc7fa4c4a
Óscar Jiménez Fernández <osc9718@gmail.com>
2020-05-26 22:19:46 +0200

    group by class count show #1

--- LR.py
+++ LR.py
@@ -10,23 +10,24 @@ if __name__ == "__main__":
     sqlContext = sql.SQLContext(sc)
     # Leer csv con columnas filtradas
     df_columns = sqlContext.read.csv("./filteredC.small.training", header=True, sep=",", inferSchema=True)
+    df_columns.groupBy("class").count().show()
     #Equilibrar clases del DF
-    c0_count = df_columns.filter(df_columns['class']==0)['class'].count()
-    c1_count = df_columns.filter(df_columns['class']==1)['class'].count()
-    print('Class 0 count: ' + str(c0_count))
-    print('Class 1 count: ' + str(c1_count))
-    tam_partition = c1_count
-    if(c0_count < c1_count):
-        tam_partition = c0_count
+    #c0_count = df_columns.filter(df_columns['class']==0)['class'].count()
+    #c1_count = df_columns.filter(df_columns['class']==1)['class'].count()
+    #print('Class 0 count: ' + str(c0_count))
+    #print('Class 1 count: ' + str(c1_count))
+    #tam_partition = c1_count
+    #if(c0_count < c1_count):
+    #    tam_partition = c0_count
     
-    df_0 = df_columns.filter(df_columns['class']==0).limit(tam_partition)
-    df_1 = df_columns.filter(df_columns['class']==1).limit(tam_partition)
+    #df_0 = df_columns.filter(df_columns['class']==0).limit(tam_partition)
+    #df_1 = df_columns.filter(df_columns['class']==1).limit(tam_partition)
 
-    df_0.head()
-    df_1.head()
+    #df_0.head()
+    #df_1.head()
 
-    print('DF0 count: ' + str(df_0['class'].count()))
-    print('DF1 count: ' + str(df_1['class'].count()))
+    #print('DF0 count: ' + str(df_0['class'].count()))
+    #print('DF1 count: ' + str(df_1['class'].count()))
 
     
     #df_columns.createOrReplaceTempView("sql_dataset_columns")
