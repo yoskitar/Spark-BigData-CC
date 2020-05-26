b7e0ef4c75f0c14ac56b51346737e758b9743c88
Óscar Jiménez Fernández <osc9718@gmail.com>
2020-05-26 21:56:23 +0200

    Equilibrando DF error #1

--- LR.py
+++ LR.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
     sqlContext = sql.SQLContext(sc)
 
     df_columns = sc.read.csv("./filteredC.small.training", header=True, sep=",", inferSchema=True)
-    # Obtenemos el tamaño de cada clase para formar un DF equilibrado
+
+
+    
     c0_count = df_columns.filter(df_columns['class']==0).shape[0]
     c1_count = df_columns.filter(df_columns['class']==1).shape[0]
     print('Class 0 count: ' + str(c0_count))
