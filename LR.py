f51e837ae4b2d651fa7673e7d489408d3070c689
Óscar Jiménez Fernández <osc9718@gmail.com>
2020-05-29 20:19:39 +0200

    Train first model LR xc3 error #2

--- LR.py
+++ LR.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
     lrModel = lr.fit(trainingData)
     trainingSummary = lrModel.summary
-    # Obtenemos las métricas del entrenamiento
+    # Obtenemos las metricas del entrenamiento
     accuracy = trainingSummary.accuracy
     falsePositiveRate = trainingSummary.weightedFalsePositiveRate
     truePositiveRate = trainingSummary.weightedTruePositiveRate
