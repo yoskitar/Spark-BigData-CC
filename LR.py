db20c8de5c234f6e53babf9c803d0e345b5f38d6
Óscar Jiménez Fernández <osc9718@gmail.com>
2020-05-30 13:29:50 +0200

    Param reg modification 2 #2

--- LR.py
+++ LR.py
@@ -4,6 +4,8 @@ from pyspark.ml.classification import LogisticRegression
 from functools import reduce
 from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
 if __name__ == "__main__":
     # create Spark context with Spark configuration
@@ -31,10 +33,13 @@ if __name__ == "__main__":
     df_test = df_balanced.sample(False, 0.2, 5)
     df_train = df_balanced.subtract(df_test)
 
+    #df_train, df_test = df_balanced.randomSplit([0.9, 0.1], seed=12345)
+
     #print('DF_Balanced count: ' + str(df_balanced.select('class').count()))
     #print('DF_Train count: ' + str(df_train.select('class').count()))
     #print('DF_Test count: ' + str(df_test.select('class').count()))
 
+    """
     print('DF_Train_1 count: ' + str(df_train.filter(df_columns['class']==1).select('class').count()))
     print('DF_Train_0 count: ' + str(df_train.filter(df_columns['class']==0).select('class').count()))
 
@@ -42,13 +47,14 @@ if __name__ == "__main__":
     print('DF_Test_0 count: ' + str(df_test.filter(df_columns['class']==0).select('class').count()))
 
     #df_train_reduced = df_train.sample(False, 1.0, 5)
-
+    """
     # Preparamos el DF para aplicar los algoritmos de MLLib
     assembler = VectorAssembler(inputCols=["PredSA_freq_global_0", "PredSA_central_-2", "PSSM_r1_3_V", "PSSM_r1_2_I", "PSSM_r1_2_W", "PSSM_r2_-4_Y"], outputCol='features')
     trainingData = assembler.transform(df_train).select("features","class").withColumnRenamed("class","label")
-
+    #testData = assembler.transform(df_test).select("features","class").withColumnRenamed("class","label")
+    
     # Entrenamos el modelo
-    lr = LogisticRegression(maxIter=100, regParam=0.1, elasticNetParam=0.8)
+    lr = LogisticRegression(maxIter=100, regParam=0.3, elasticNetParam=0.8)
     lrModel = lr.fit(trainingData)
     trainingSummary = lrModel.summary
 
@@ -58,4 +64,16 @@ if __name__ == "__main__":
     # Obtenemos las metricas del entrenamiento
     trainingSummary.roc.show()
     print("areaUnderROC: " + str(trainingSummary.areaUnderROC))
+    
+    """
+    lr = LogisticRegression(maxIter=10)
+    paramGrid = ParamGridBuilder().addGrid(lr.regParam, [0.1, 0.01]).addGrid(lr.fitIntercept, [False, True]).addGrid(lr.elasticNetParam, [0.0, 0.5, 1.0]).build()
+    tvs = TrainValidationSplit(estimator=lr,
+                           estimatorParamMaps=paramGrid,
+                           evaluator=BinaryClassificationEvaluator(),
+                           # 80% of the data will be used for training, 20% for validation.
+                           trainRatio=0.8)
+    model = tvs.fit(trainingData)
+    model.transform(testData).select("features", "label", "prediction").show()
+    """
     sc.stop()
